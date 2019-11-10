6bd83bef46f8ba1a7f321b9148ba21ca00d32ac3
Devlin McClure <mccluredt@gmail.com>
2019-11-10 11:04:33 -0500

    Add comments to MovingObject script

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public abstract class MovingObject : MonoBehaviour
 {
+    //time it will take to move
     public float moveTime = 0.1f;
+    //Layer for collision checks
     public LayerMask blockingLayer;
 
+    //Components the object has
     private BoxCollider2D boxCollider;
     private Rigidbody2D rb2D;
+    //Float to make calculations for moving easier
     private float inverseMoveTime;
 
 
@@ -17,9 +21,11 @@ public abstract class MovingObject : MonoBehaviour
     {
         boxCollider = GetComponent<BoxCollider2D>();
         rb2D = GetComponent<Rigidbody2D>();
+        //Get the inverse of moveTime to make calculations easier
         inverseMoveTime = 1f / moveTime;
     }
 
+    //Return true if able to move, false if can't move
     protected bool Move(int xDir, int yDir, out RaycastHit2D hit)
     {
         Vector2 start = transform.position;
@@ -29,23 +35,31 @@ public abstract class MovingObject : MonoBehaviour
         hit = Physics2D.Linecast(start, end, blockingLayer);
         boxCollider.enabled = true;
 
+        //Check if anything was hit
         if(hit.transform == null)
         {
+            //Start moving if nothing was hit
             StartCoroutine(SmoothMovement(end));
             return true;
         }
-
+        //Something was hit
         return false;
     }
 
+    //Co-routine for moving units from one space to the next
     protected IEnumerator SmoothMovement(Vector3 end)
     {
+        //Calculate remaining distance to end point
         float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
 
+        //While distance is greater than a small amount
         while (sqrRemainingDistance > float.Epsilon)
         {
+            //Find place that is closer to end point
             Vector3 newPostion = Vector3.MoveTowards (rb2D.position, end, inverseMoveTime * Time.deltaTime);
+            //Move ot new position
             rb2D.MovePosition(newPostion);
+            //Find the remaining distance
             sqrRemainingDistance = (transform.position - end).sqrMagnitude;
             yield return null;
         }
@@ -54,17 +68,23 @@ public abstract class MovingObject : MonoBehaviour
     protected virtual void AttemptMove <T> (int xDir, int yDir)
         where T : Component
     {
+        //Whatever the linecast hits
         RaycastHit2D hit;
+        //Set to whatever the outcome of Move was
         bool canMove = Move(xDir, yDir, out hit);
         
+        //Check if nothing was hit
         if(hit.transform == null)
         {
+            //Don't do anything else
             return;
         }    
 
+        //Get a reference to the component of type T attached to the object hit
         T hitComponent = hit.transform.GetComponent<T>();
         if (!canMove && hitComponent != null)
         {
+            //call OnCantMove if the MovingObject hit something
             OnCantMove(hitComponent);
         }
     }
