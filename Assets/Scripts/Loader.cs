d7d905175a2a024e267618ffd3eef1187e1618ce
Devlin McClure <mccluredt@gmail.com>
2019-11-10 10:28:07 -0500

    Make the GameManager a prefab to be loaded in on setup

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+
+    public GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (GameManager.instance == null){
+            Instantiate(gameManager);
+        }
+    }
+}
